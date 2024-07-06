11e47e1ca8ecec5897e5c10fe40dbce943b850d2
Gabriel Acosta <acostadariogabriel@gmail.com>
2024-07-06 18:34:58 -0300

    feat: add GPU temp

---
+++ components/gpu.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../slstatus.h"
+#include "../util.h"
+
+const char *gpu_temp(const char *unused) {
+
+  int temp;
+  const char *result;
+  const char *cmd =
+      "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader";
+
+  result = run_command(cmd);
+  temp = atoi(result);
+
+  return bprintf("%ju", temp);
+}
